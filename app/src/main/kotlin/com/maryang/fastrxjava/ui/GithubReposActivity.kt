2f3addf681eea7a9bbd04b2a4d6968c893aefc57
jskim <kjs6030@naver.com>
2019-06-29 18:50:25 +0900

    first_week_excersice

--- app/src/main/kotlin/com/maryang/fastrxjava/ui/GithubReposActivity.kt
+++ app/src/main/kotlin/com/maryang/fastrxjava/ui/GithubReposActivity.kt
@@ -1,10 +1,21 @@
 package com.maryang.fastrxjava.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.maryang.fastrxjava.R
+import com.maryang.fastrxjava.base.BaseApplication
+import com.maryang.fastrxjava.entity.GithubRepo
+import com.maryang.fastrxjava.entity.User
+import io.reactivex.SingleObserver
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.observers.DisposableCompletableObserver
+import io.reactivex.observers.DisposableMaybeObserver
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_github_repos.*
 
 
@@ -32,16 +43,94 @@ class GithubReposActivity : AppCompatActivity() {
     private fun load(showLoading: Boolean = false) {
         if (showLoading)
             showLoading()
-        viewModel.getGithubRepos(
-            {
-                hideLoading()
-                adapter.items = it
-            },
-            {
-                hideLoading()
-            }
-        )
+        // back 버튼을 누르면 종료가 됩니ㅏㄷ.
+        // 액티비티에 있는 Observable이 구독하는 Observer는 액티비티의 Context를 참조(view)하기 때문에
+        // GC가 수직을 못한다.
+        // 액티비티는 종료가 되어야하는 데, Dispose가 되어있지 않은 Observer에 wkqgu dl
+        viewModel.getGithubRepos()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : DisposableSingleObserver<List<GithubRepo>>() {
+                override fun onSuccess(t: List<GithubRepo>) {
+                    hideLoading()
+                    adapter.items = t
+                }
+
+                override fun onError(e: Throwable) {
+                    hideLoading()
+                }
+            })
     }
+
+    private fun load2(showLoading: Boolean = false) {
+        if (showLoading) {
+            showLoading()
+        }
+
+        viewModel.getUser()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object:DisposableMaybeObserver<User>() {
+                override fun onSuccess(t: User) {
+                    //null이 아닌것만 온다.
+                }
+
+                override fun onComplete() {
+                    //null이 발생했을
+                }
+
+                override fun onError(e: Throwable) {
+                }
+            })
+
+    }
+
+    private fun load3(showLoading: Boolean = false) {
+        viewModel.updateUser()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object:DisposableCompletableObserver(){
+                override fun onComplete() {
+                }
+
+                override fun onError(e: Throwable) {
+                }
+            })
+    }
+
+    private fun load4(showLoading: Boolean = false) {
+        // Single.zip: 두 개의 동작을 동시에 처리하고, 동작이 모두 끝나면 subscribe
+        // map: element를 변경
+        // flatmap:  객체 자체를 변경
+        viewModel.getGithubRepos()
+            .subscribeOn(Schedulers.io())
+            .toMaybe()
+            .doOnSuccess {
+                //getGithubRepos 종료되면 아래의 로그가 블립니다.
+                Log.d(BaseApplication.TAG, "getGitHubRepos: $it")
+            }
+            .flatMap {
+                viewModel.getUser()
+            }
+            .doOnSuccess {
+                //getUser가 종료되면 로그가 불림
+                Log.d(BaseApplication.TAG, "getUser: $it")
+            }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object:DisposableMaybeObserver<User>() {
+                override fun onSuccess(t: User) {
+                    //null이 아닌것만 온다.
+                }
+
+                override fun onComplete() {
+                    //null이 발생했을
+                }
+
+                override fun onError(e: Throwable) {
+                }
+            })
+    }
+
 
     private fun showLoading() {
         loading.visibility = View.VISIBLE
--- app/src/main/kotlin/com/maryang/fastrxjava/ui/GithubReposViewModel.kt
+++ app/src/main/kotlin/com/maryang/fastrxjava/ui/GithubReposViewModel.kt
@@ -1,30 +1,18 @@
 package com.maryang.fastrxjava.ui
 
 import com.maryang.fastrxjava.data.repository.GithubRepository
-import com.maryang.fastrxjava.data.source.DefaultCallback
 import com.maryang.fastrxjava.entity.GithubRepo
-import retrofit2.Call
-import retrofit2.Response
+import com.maryang.fastrxjava.entity.User
+import io.reactivex.Completable
+import io.reactivex.Maybe
+import io.reactivex.Single
 
 class GithubReposViewModel {
     private val repository = GithubRepository()
 
-    fun getGithubRepos(
-        onResponse: (List<GithubRepo>) -> Unit,
-        onFailure: (Throwable) -> Unit
-    ) {
-        repository.getGithubRepos().enqueue(object : DefaultCallback<List<GithubRepo>>() {
-            override fun onResponse(
-                call: Call<List<GithubRepo>>,
-                response: Response<List<GithubRepo>>
-            ) {
-                onResponse(response.body() ?: emptyList())
-            }
+    fun getGithubRepos() : Single<List<GithubRepo>> = repository.getGithubRepos()
 
-            override fun onFailure(call: Call<List<GithubRepo>>, t: Throwable) {
-                super.onFailure(call, t)
-                onFailure(t)
-            }
-        })
-    }
+    fun getUser(): Maybe<User> = repository.getUser()
+
+    fun updateUser():Completable = repository.updateUser()
 }
