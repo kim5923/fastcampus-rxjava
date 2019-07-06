afc726244745e4d941070f78d96c7c2a1f413e10
jskim <kjs6030@naver.com>
2019-07-06 16:52:30 +0900

    second_week

---
+++ app/src/main/kotlin/com/maryang/fastrxjava/entity/Sample.kt
@@ -0,0 +1,6 @@
+package com.maryang.fastrxjava.entity
+
+class Sample {
+    var data: Int = 0
+}
+
--- app/src/main/kotlin/com/maryang/fastrxjava/ui/GithubReposViewModel.kt
+++ app/src/main/kotlin/com/maryang/fastrxjava/ui/GithubReposViewModel.kt
@@ -2,6 +2,8 @@ package com.maryang.fastrxjava.ui
 
 import com.maryang.fastrxjava.data.repository.GithubRepository
 import com.maryang.fastrxjava.entity.GithubRepo
+import com.maryang.fastrxjava.util.Operators
+import com.maryang.fastrxjava.util.applySchedulersExtension
 import io.reactivex.Completable
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -30,6 +32,56 @@ class GithubReposViewModel {
         }
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
+
+        // compose
+        return Single.create<List<GithubRepo>> { emitter ->
+            repository.searchGithubRepos(searchText)
+                .subscribe({
+                    Completable.merge(
+                        it.map { repo ->
+                            checkStar(repo.owner.userName, repo.name)
+                                .doOnComplete { repo.star = true }
+                                .onErrorComplete()
+                        }
+                    ).subscribe {
+                        emitter.onSuccess(it)
+                    }
+                }, {})
+        }
+            .compose(Operators.applySchedulers())
+
+        // extension 활용
+        return Single.create<List<GithubRepo>> { emitter ->
+            repository.searchGithubRepos(searchText)
+                .subscribe({
+                    Completable.merge(
+                        it.map { repo ->
+                            checkStar(repo.owner.userName, repo.name)
+                                .doOnComplete { repo.star = true }
+                                .onErrorComplete()
+                        }
+                    ).subscribe {
+                        emitter.onSuccess(it)
+                    }
+                }, {})
+        }
+            .applySchedulersExtension()
+
+        // applySchedulers 함수를 통해 반복적인 코드 제거
+//        return Operators.applySchedulers(Single.create<List<GithubRepo>> { emitter ->
+//            repository.searchGithubRepos(searchText)
+//                .subscribe({
+//                    Completable.merge(
+//                        it.map { repo ->
+//                            checkStar(repo.owner.userName, repo.name)
+//                                .doOnComplete { repo.star = true }
+//                                .onErrorComplete()
+//                        }
+//                    ).subscribe {
+//                        emitter.onSuccess(it)
+//                    }
+//                }, {})
+//        })
     }
 
     private fun checkStar(owner: String, repo: String): Completable =
