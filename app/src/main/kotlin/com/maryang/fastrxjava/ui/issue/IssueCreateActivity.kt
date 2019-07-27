a17b3482b02235948c5d7d13d11197c96facdb41
jskim <kjs6030@naver.com>
2019-07-27 15:38:32 +0900

    final week exercise

--- app/src/main/kotlin/com/maryang/fastrxjava/ui/issue/IssueCreateActivity.kt
+++ app/src/main/kotlin/com/maryang/fastrxjava/ui/issue/IssueCreateActivity.kt
@@ -4,17 +4,22 @@ import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import com.jakewharton.rxbinding3.view.clicks
+import com.jakewharton.rxbinding3.widget.textChanges
 import com.maryang.fastrxjava.R
 import com.maryang.fastrxjava.base.BaseActivity
 import com.maryang.fastrxjava.entity.GithubRepo
 import com.maryang.fastrxjava.entity.Issue
 import com.maryang.fastrxjava.event.DataObserver
 import com.maryang.fastrxjava.observer.DefaultSingleObserver
+import com.maryang.fastrxjava.util.RxBindingSample
 import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.activity_issue_create.*
 import org.jetbrains.anko.backgroundColorResource
 import org.jetbrains.anko.intentFor
 import org.jetbrains.anko.sdk21.listeners.onClick
+import org.jetbrains.anko.sdk21.listeners.textChangedListener
 import org.jetbrains.anko.toast
 import java.util.concurrent.TimeUnit
 
@@ -50,19 +55,25 @@ class IssueCreateActivity : BaseActivity() {
     }
 
     private fun setEditText() {
-        titleText.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(text: Editable?) {
-                text?.let {
-                    enableComplete(it.isNotEmpty() && bodyText.length() > 0)
-                }
+        Observable.merge(
+            titleText.textChanges(), bodyText.textChanges()
+        )
+            .subscribe {
+                enableComplete(it.isNotEmpty() && titleText.length() > 0)
             }
-
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-        })
+//        titleText.textChanges()
+//                .subscribe { text ->
+//                    text?.let {
+//                        enableComplete(it.isNotEmpty() && bodyText.length() > 0)
+//                    }
+//
+//                }
+//        bodyText.textChanges()
+//                .subscribe { text ->
+//                    text?.let {
+//                        enableComplete(it.isNotEmpty() && titleText.length() > 0)
+//                    }
+//                }
 
         bodyText.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(text: Editable?) {
@@ -85,22 +96,35 @@ class IssueCreateActivity : BaseActivity() {
             if (enable) R.color.colorPrimary else R.color.grey_500
     }
 
-    private var isCompleteClicking = false
     private fun setOnClickListener() {
-        complete.onClick {
-            if (isCompleteClicking) return@onClick
-            isCompleteClicking = true
-            Observable.timer(1, TimeUnit.SECONDS)
-                .subscribe { isCompleteClicking = false }
-
-            viewModel.create(repo, titleText.text.toString(), bodyText.text.toString())
-                .subscribe(object : DefaultSingleObserver<Issue>() {
-                    override fun onSuccess(t: Issue) {
-                        DataObserver.post(t)
-                        toast("이슈를 생성하였습니다")
-                        finish()
-                    }
-                })
-        }
+//        complete.clicks()
+        RxBindingSample.click(complete)
+            .throttleFirst(1, TimeUnit.SECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                viewModel.create(repo, titleText.text.toString(), bodyText.text.toString())
+                    .subscribe(object : DefaultSingleObserver<Issue>() {
+                        override fun onSuccess(t: Issue) {
+                            DataObserver.post(t)
+                            toast("이슈를 생성하였습니다")
+                            finish()
+                        }
+                    })
+            }
+//        complete.onClick {
+//            if (isCompleteClicking) return@onClick
+//            isCompleteClicking = true
+//            Observable.timer(1, TimeUnit.SECONDS)
+//                .subscribe { isCompleteClicking = false }
+//
+//            viewModel.create(repo, titleText.text.toString(), bodyText.text.toString())
+//                .subscribe(object : DefaultSingleObserver<Issue>() {
+//                    override fun onSuccess(t: Issue) {
+//                        DataObserver.post(t)
+//                        toast("이슈를 생성하였습니다")
+//                        finish()
+//                    }
+//                })
+//        }
     }
 }
